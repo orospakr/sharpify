e745abf39b832a92c504824688c7fe9a3fe19579
Andrew Clunis <andrew@orospakr.ca>
2012-11-06 16:05:41 -0500

    Missed adding a file. That always happens.

---
+++ ShopifyAPIAdapterLibrary.Tests/RestResourceTest.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ShopifyAPIAdapterLibrary.Tests
+{
+    public class Robot {
+
+    }
+
+    public class RestResourceTest
+    {
+        public RestResourceTest ()
+        {
+            // var resource = new RestResource<Robot>
+            // TODO start here and mock out ShopifyApiAdapter with FakeItEasy
+        }
+    }
+}
+
