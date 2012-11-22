52073a13355af475e922455254eab697b556d96e
Andrew Clunis <andrew@orospakr.ca>
2012-11-22 16:12:27 -0500

    Propify.rb now writes changes to the cs files.
    However, it's broken on account of also changing the flat POCOs
    and not just the ShopifyResourceModels.

--- ShopifyAPIAdapterLibrary/ShopifyAPIAdapterLibrary.Models/propify.rb
+++ ShopifyAPIAdapterLibrary/ShopifyAPIAdapterLibrary.Models/propify.rb
@@ -1,10 +1,19 @@
 #!/usr/bin/env ruby
 
+# Replace simple { get; set; } flat properties with
+# explicitly generated (and sadly boilerplatey) prop
+# getter and setters that call SetProperty() in order
+# to make our IPropertyChangedRegime work.
+
+# Obviously a bit naiively and regexy, but it works.
+# I didn't want to use Fody or a Weaver because of lack
+# of MonoDevelop/xbuild support.
+
 models_path = File.dirname(__FILE__)
 
 puts "Transmogrifying any default properties to PropertySet() in: #{models_path}"
 
-regex = /public\s([a-zA-Z\?]*)\s([a-zA-Z]*)\s{ get; set; }/
+regex = /public\s([a-zA-Z\?\<\>]*)\s([a-zA-Z]*)\s{ get; set; }/
 
 # match group \1 is the property type, \2 is the property name
 replace_with = <<-eos
@@ -24,10 +33,14 @@ Dir.glob(cs_glob) do |cs_file|
   puts "... #{cs_file}"
   cs_file_path = File.join(models_path, cs_file)
 
-  cs_file_contents = File.read(cs_file_path)
-  puts "... ... #{cs_file_contents.length}"
-
+  cs_file_contents = ""
+  fd = File.open(cs_file_path, "rb") do |fd|
+    cs_file_contents = fd.read()
+  end
   
   transmog = cs_file_contents.gsub(regex, replace_with)
-  puts transmog
+
+  fd = File.open(cs_file_path, "wb") do |fd|
+    fd.write(transmog)
+  end
 end
