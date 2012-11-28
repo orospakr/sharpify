af56e5c9f724aad319d86dd452644b0e891df2c3
Andrew Clunis <andrew@orospakr.ca>
2012-11-27 20:03:12 -0500

    Throw an exception of [CallerMemberName] misbehaves.

--- ShopifyAPIAdapterLibrary/ShopifyAPIAdapterLibrary.Models/ShopifyResourceModel.cs
+++ ShopifyAPIAdapterLibrary/ShopifyAPIAdapterLibrary.Models/ShopifyResourceModel.cs
@@ -14,8 +14,13 @@ namespace ShopifyAPIAdapterLibrary.Models
 
         private HashSet<string> Dirty;
 
-        public void SetProperty<T>(ref T field, T value, [CallerMemberName] string name = "")
+        public void SetProperty<T>(ref T field, T value, [CallerMemberName] string name = null)
         {
+            if (name == null)
+            {
+                throw new ShopifyConfigurationException("Field name is coming up null in SetProperty.  Something's wrong.");
+            }
+            Console.WriteLine("SETTING PROPERTY {0} to {1}", name, value);
             // thanks to http://danrigby.com/2012/03/01/inotifypropertychanged-the-net-4-5-way/
             if (!EqualityComparer<T>.Default.Equals(field, value))
             {
